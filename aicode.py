7f3775cc3818573e84f7db13339b2f21418de98b
Tarandeep Singh <tarandeeps1197@gmail.com>
2018-07-21 17:15:17 +0530

    Update aicode.py

--- aicode.py
+++ aicode.py
@@ -1,6 +1,10 @@
 import nltk
 tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
+words = nltk.tokenize.word_tokenize
 para = input()
 sent = tokenizer.tokenize(para)
 for sentence in sent:
     print(sentence)
+    print(words.sentence)
+    
+
